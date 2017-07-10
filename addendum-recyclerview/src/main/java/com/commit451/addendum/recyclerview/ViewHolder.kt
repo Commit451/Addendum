39bc4b7dcc2fd4b0343f54f3be79d956a38b5a67
John Carlson <jawnnypoo@gmail.com>
2017-07-10 08:35:06 -0500

    bindView for ViewHolder

--- addendum-recyclerview/src/main/java/com/commit451/addendum/recyclerview/ViewHolder.kt
+++ addendum-recyclerview/src/main/java/com/commit451/addendum/recyclerview/ViewHolder.kt
@@ -2,7 +2,9 @@ package com.commit451.addendum.recyclerview
 
 import android.content.Context
 import android.content.res.Resources
+import android.support.annotation.IdRes
 import android.support.v7.widget.RecyclerView
+import android.view.View
 
 fun RecyclerView.ViewHolder.resources(): Resources {
     return itemView.context.resources
@@ -10,4 +12,9 @@ fun RecyclerView.ViewHolder.resources(): Resources {
 
 fun RecyclerView.ViewHolder.context(): Context {
     return itemView.context
+}
+
+fun <T : View> RecyclerView.ViewHolder.bindView(@IdRes id: Int): Lazy<T> {
+    @Suppress("UNCHECKED_CAST")
+    return lazy(LazyThreadSafetyMode.NONE) { itemView.findViewById(id) as T }
 }
