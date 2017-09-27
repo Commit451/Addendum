1dcdfd52aaebff5ed9d2076321908111297ac9fc
John Carlson <jawnnypoo@gmail.com>
2017-09-27 13:29:30 -0500

    Asset as string

---
+++ addendum/src/main/java/com/commit451/addendum/AssetManager.kt
@@ -0,0 +1,13 @@
+package com.commit451.addendum
+
+import android.content.res.AssetManager
+import java.nio.charset.Charset
+
+fun AssetManager.openAsString(fileName: String): String? {
+    val `is` = open(fileName)
+    val size = `is`.available()
+    val buffer = ByteArray(size)
+    `is`.read(buffer)
+    `is`.close()
+    return String(buffer, Charset.forName("UTF-8"))
+}
