22f74cabf3b1208f7b00ebff2727ef6215e8b884
John <jawnnypoo@gmail.com>
2017-10-31 14:33:36 -0500

    Potential fix for dp conversion being wrong

--- addendum/src/main/java/com/commit451/addendum/Float.kt
+++ addendum/src/main/java/com/commit451/addendum/Float.kt
@@ -1,8 +1,7 @@
 package com.commit451.addendum
 
 import android.content.Context
-import android.util.DisplayMetrics
 
 fun Float.dp(context: Context): Float {
-    return this * (Addendum.xdpi(context) / DisplayMetrics.DENSITY_DEFAULT)
+    return this * context.resources.displayMetrics.density
 }
